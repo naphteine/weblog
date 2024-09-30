b16816752ffd4a143d2a5fd839a65aa675394e99
naphteine <58526116+naphteine@users.noreply.github.com>
2024-10-01 01:29:07 +0300

    Fix script tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -30,11 +31,11 @@ export default function RootLayout({
       >
         {children}
       </body>
-      <script
+      <Script
         defer
         src="https://umi.gokay.works/script.js"
         data-website-id="1e1dcdd7-fb77-452d-8c30-9b8bd0edde1d"
-      ></script>
+      ></Script>
     </html>
   );
 }
