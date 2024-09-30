59a8b000f5d788f9fdf197cb9b9434dd1fc8ac29
naphteine <58526116+naphteine@users.noreply.github.com>
2024-10-01 02:30:00 +0300

    index page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,23 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
-      <header></header>
-      <main></main>
-      <footer></footer>
+      <header className="mx-8 my-8">
+        <h1 className="text-4xl">Personal weblog</h1>
+      </header>
+      <main className="mx-8 my-8">
+        <h2 className="text-2xl font-bold">List of posts</h2>
+      </main>
+      <footer className="mx-8 my-8">
+        <Link className="underline" href="https://gokay.works">
+          Gökay Gültekin
+        </Link>{" "}
+        &copy; 2019-2024{" | "}
+        <Link className="underline" href="https://github.com/naphteine/weblog">
+          Source code
+        </Link>
+      </footer>
     </>
   );
 }
